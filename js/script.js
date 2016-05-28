9e93d1718f10674e8c3e2de6bbec970b74dd82f9
Théo Zimmermann <theozimm2010@gmail.com>
2016-05-28 15:07:56 +0200

    fulfilling the basic scope

--- js/script.js
+++ js/script.js
@@ -1,15 +1,66 @@
 var maxLength = 20;
 
-/* writing HTML */
+/* WRITING THE FIRST PAGE *------------------------------------------------------------------------------*/
+
+document.write(
+    ' <div data-role="page" id="home">' +
+    '    <div data-role="header">' +
+    '        <h1>MOB1</h1>' +
+    '    </div>' +
+    '    <div data-role="content" >' +	
+    '        <ul data-role="listview" data-autodivider="true" data-inset="true" data-theme="a">' +
+    '            <li data-role="list-divider"> Choose a category ' +
+    '            </li>' +
+    '            <li>' +
+    '                <img src="img/LoL_icon.png" alt="LoL" class="ui-li-icon"/>' +
+    '                <a href="#list" data-rel="list">lol</a>' +
+    '            </li>' +
+    '            <li>' +
+    '                <img src="img/dota_2_icon_by_benashvili-d6w0695.png" alt="dota2" class="ui-li-icon"/>' +
+    '                <a href="#list">dota2</a>' +
+    '            </li>' +
+    '            <li>' +
+    '                <img src="img/csgo-logo.png" alt="csgo" class="ui-li-icon"/>' +
+    '                <a href="#list">csgo</a>' +
+    '            </li>' +
+    '            <li>' +
+    '                <img src="img/hots.png" alt="hots" class="ui-li-icon"/>' +
+    '                <a href="#list">hots</a>' +
+    '            </li>' +
+    '            <li>' +
+    '                <img src="img/hs_icon.png" alt="hs" class="ui-li-icon"/>' +
+    '                <a href="#list">hearthstone</a>' +
+    '            </li>' +
+    '            <li>' +
+    '                <img src="img/starcraft-ii-icon.png" alt="sc2" class="ui-li-icon"/>' +
+    '                <a href="#list">sc2</a>' +
+    '            </li>' +
+    '            <li>' +
+    '                <img src="img/3xhumed-Mega-Games-Pack-33-Call-of-Duty-Modern-Warfare-2-8.ico" alt="cod+" class="ui-li-icon"/>' +
+    '                <a href="#list">cod</a>' +
+    '            </li>' +
+    '        </ul>' +
+    '    </div>' +
+    '</div>'
+);
+
+/* WRITING THE SECOND (DYNAMIC) PAGE *-----------------------------------------------------------------------*/
+
 document.write(
     '<div data-role="page" id="list">' +
-    '  <div data-role="header" data-position="fixed">' +
-    '    <h1><span id="widgetTitle">...</span> ' +
+    '  <div data-role="header" data-position="inline">' +
+    '    <a href="#home" data-role="button" data-icon="home">Home</a>' +
+    '    <h1><span>TheScore Esport RSS Reader </span> ' +
     '      <span style="font-size: x-small"></span></h1>' +
+    '    <a href="#" id="openButton' + i + '" data-role="button" data-icon="plus"' +
+    '      class="ui-btn-right" rel="external">Open</a>' +
     '  </div>' +
     '  <div data-role="content">' +
     '    <ul data-role="listview" data-inset="true" data-filter="true" data-theme="a" id="articleList">'
 );
+
+/* WRITE ARTICLES LINKS UNTIL MAXLENGTH IS ACHIEVED------------------------------------------------------------------*/
+
 for(var i=1; i<=maxLength; i++){
     document.write(
         '<li id="list' + i + '"><a href="#article' + i + '" id="link' + i + '">&nbsp;</a></li>'
@@ -20,11 +71,14 @@ document.write(
     '  </div>' +
     '</div>'
 );
+
+/* WRITE ARTICLES UNTIL MAXLENGTH IS ACHIEVED------------------------------------------------------------------*/
+
 for(i=1; i<=maxLength; i++){
     document.write(
         '<div data-role="page" id="article' + i + '">' +
         '  <div data-role="header" data-position="inline">' +
-        '    <a href="#list" data-role="button" data-icon="home" data-back="true">Home</a>' +
+        '    <a href="#home" data-role="button" data-icon="home">Home</a>' +
         '    <h1 id="articleHeader' + i + '">&nbsp;</h1>' +
         '    <a href="#" id="openButton' + i + '" data-role="button" data-icon="plus"' +
         '      class="ui-btn-right" rel="external">Open</a>' +
@@ -42,29 +96,37 @@ for(i=1; i<=maxLength; i++){
     );
 }
 
-
+/* DYNAMIC URL DEPENDING ON WHERE YOU'RE CLICKING ON THE FIRST PAGE */
+/* CLICK CONTENT APPEND ON THE PROCESSED URL */
 $(function(){
-    var cat = "sc2";
-    getOnlineFeed('http://feeds.thescoreesports.com/'+cat+'.rss');
+    var cat ="";
     $("#home li a").click(function(){
         cat = ($(this).text());
-       console.log(cat);
+        getOnlineFeed('http://feeds.thescoreesports.com/'+cat+'.rss');
     });
 });
 
 /* functions */
 var listEntries = function(json) {
+    
     if (!json.responseData.feed.entries) return false;
     $('#widgetTitle').text(json.responseData.feed.title);
     var articleLength =json.responseData.feed.entries.length;
+    console.log(articleLength);
     articleLength = (articleLength > maxLength) ? maxLength : articleLength;
     for (var i = 1; i <= articleLength ; i++) {
+        
+        /* FOR EACH ARTICLES THERE IS */
         var entry = json.responseData.feed.entries[i-1];
-        $('#link' + i).text(entry.title);
-        $('#articleHeader' + i).text(entry.title);
-        $('#openButton' + i).attr('href', entry.link);
-        $('#articleContent' + i).append(entry.content);
+        
+        /* A LINK TO IT */          $('#link' + i).text(entry.title);
+        /* A HEADER TO DISPLAY */   $('#articleHeader' + i).text(entry.title);
+        /* A LINK TO IT */          $('#openButton' + i).attr('href', entry.link);
+        /* THE ARTICLE'S CONTENT */ $('#articleContent' + i).append(entry.content);
     }
+
+    // NAVIGATE THROUGH ARTICLES OF THE SAME CATEGORY
+    
     $('#article1 .prevButton').remove();
     $('#article' + articleLength + ' .nextButton').remove();
     if (articleLength < maxLength) {
@@ -74,6 +136,8 @@ var listEntries = function(json) {
         }
     }
 };
+
+/* PROCESSSING OF THE URL */
 var getOnlineFeed = function(url) {
     var script = document.createElement('script');
     script.setAttribute('src', 'http://ajax.googleapis.com/ajax/services/feed/load?callback=listEntries&hl=ja&output=json-in-script&q='
@@ -81,10 +145,7 @@ var getOnlineFeed = function(url) {
                         + '&v=1.0&num=' + maxLength);
     script.setAttribute('type', 'text/javascript');
     document.documentElement.firstChild.appendChild(script);
-};
-var getOfflineFeed = function(url) {
-    var script = document.createElement('script');
-    script.setAttribute('src', url);
-    script.setAttribute('type', 'text/javascript');
-    document.documentElement.firstChild.appendChild(script);
+    // Append tu script dans le <head> du html
+
+
 };
--- server/rss/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-if (array_key_exists("rssUrl", $_REQUEST)){
-	header("content-type: application/xml");
-	$xml = file_get_contents($_REQUEST["rssUrl"]); 
-	print($xml);
-}
-?>
